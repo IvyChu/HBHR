84d9f9b5f72d4d3a9a545316c5f8938be9f774f1
IvyChu <ivanica@gmail.com>
2023-04-18 19:58:44 +0200

    Setup added then commented out

--- hbhr/admin/routes.py
+++ hbhr/admin/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, Blueprint, flash
 from flask_security import current_user, roles_accepted
 from hbhr.models import User, Service
-from hbhr import db, user_datastore
+from hbhr import log, db, user_datastore
 from hbhr.admin.forms import ServiceForm
 
 admin = Blueprint('admin', __name__)
@@ -12,25 +12,29 @@ admin = Blueprint('admin', __name__)
 def home():
     return render_template('admin/admin.html', title='Admin')
 
+
 @admin.route("/admin/services/<int:service_id>/edit", methods=['GET'])
 @roles_accepted('admin')
 def edit_service(service_id):
     service = Service.query.get_or_404(service_id)
-    form = ServiceForm()    
+    form = ServiceForm()
     form.name.data = service.name
     form.description.data = service.description
     return render_template('admin/service_edit.html', service=service, form=form)
+
 
 @admin.route("/admin/services/", methods=['POST'])
 @roles_accepted('admin')
 def add_service():
     form = ServiceForm()
     if form.validate_on_submit():
-        service = Service(name=form.name.data, description=form.description.data)
+        service = Service(name=form.name.data,
+                          description=form.description.data)
         db.session.add(service)
         db.session.commit()
         return render_template('admin/service_row.html', service=service)
     return "Error encountered while adding a service."
+
 
 @admin.route("/admin/services/<int:service_id>", methods=['DELETE'])
 @roles_accepted('admin')
@@ -38,7 +42,7 @@ def delete_service(service_id):
     # Get the service with the given ID or return a 404 error if not found
     service = Service.query.get_or_404(service_id)
 
-    # Delete the service 
+    # Delete the service
     db.session.delete(service)
 
     # Commit the changes to the database
@@ -46,21 +50,21 @@ def delete_service(service_id):
 
     return ''
 
+
 @admin.route("/admin/services/<int:service_id>/status", methods=['PUT'])
 @roles_accepted('admin')
 def toggle_service_status(service_id):
     # Get the service with the given ID or return a 404 error if not found
     service = Service.query.get_or_404(service_id)
-    
+
     # Toggle the status of the service (i.e. change from active to inactive or vice versa)
     service.toggle_status()
-    
+
     # Commit the changes to the database
     db.session.commit()
-    
+
     # Render the template for the updated service row
     return render_template('admin/service_row.html', service=service)
-
 
 
 @admin.route("/admin/services/<int:service_id>", methods=['GET', 'PUT'])
@@ -86,3 +90,26 @@ def services():
     form = ServiceForm()
 
     return render_template('admin/services.html', title='Edit services', services=services, form=form)
+
+
+# @admin.route("/admin/setup")
+# def setup():
+
+#     chu = User.query.filter(User.username == 'vchu').first()
+#     log.info(chu)
+
+#     user_datastore.create_role(name='admin', description='Administrator')
+#     log.info("created admin role")
+
+#     admin_role = user_datastore.find_role("admin")
+#     log.info("found admin role")
+
+#     user_datastore.add_role_to_user(chu, admin_role)
+#     log.info("added admin role to chu")
+
+#     # user_datastore.activate_user(chu)
+
+#     db.session.commit()
+#     log.info("committed to db")
+
+#     return render_template('admin/setup.html', title='Admin')
